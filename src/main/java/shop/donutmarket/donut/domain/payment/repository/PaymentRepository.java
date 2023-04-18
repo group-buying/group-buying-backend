38625160621882807f912b3851d8938647a7dbbb
K-Minho <alsgh9979@naver.com>
2023-04-18 09:52:23 +0900

    Feat: Payment 추가

---
+++ src/main/java/shop/donutmarket/donut/domain/payment/repository/PaymentRepository.java
@@ -0,0 +1,11 @@
+package shop.donutmarket.donut.domain.payment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import shop.donutmarket.donut.domain.payment.model.Payment;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long>{
+    
+}
