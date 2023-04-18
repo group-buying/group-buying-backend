3b1185a8142fee3d30c2a7a7193d08258ce9e8b3
K-Minho <alsgh9979@naver.com>
2023-04-18 15:23:12 +0900

    Feat: WishlistTest 추가

---
+++ src/test/java/shop/donutmarket/donut/domain/wishlist/WishlistRepositoryTest.java
@@ -0,0 +1,67 @@
+package shop.donutmarket.donut.domain.wishlist;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import shop.donutmarket.donut.domain.wishlist.model.Wishlist;
+import shop.donutmarket.donut.domain.wishlist.repository.WishlistRepository;
+
+@DataJpaTest
+@Transactional
+public class WishlistRepositoryTest {
+    
+    @Autowired
+    private WishlistRepository wishlistRepository;
+
+    @BeforeEach
+    void setUp(){
+        Wishlist wishlist = Wishlist.builder().id(1L).userId(1L).boardId(1L).build();
+        wishlistRepository.save(wishlist);
+    }
+    
+    
+    @Test
+    void findById_Test(){
+        // given
+        Long id = 1L;
+        
+        // when
+        Optional<Wishlist> wishlist = wishlistRepository.findById(id);
+        
+        // then
+        assertNotNull(wishlist);
+    }
+    
+    @Test
+    void save_Test() {
+        // given
+        Long id = 2L;
+        Wishlist wishlist = Wishlist.builder().id(id).userId(1L).boardId(2L).build();
+        
+        // when
+        wishlistRepository.save(wishlist);
+        
+        // then
+        assertNotNull(wishlistRepository.findById(id));
+    }
+
+    @Test
+    void deleteById_Test(){
+        // given
+        Long id = 1L;
+        
+        // when
+        wishlistRepository.deleteById(id);
+
+        // then
+        assertEquals(Optional.empty(), wishlistRepository.findById(id));
+    }
+}
