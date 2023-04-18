1532bd7381d749dcde85fa59e0c65216e2a4bea1
K-Minho <alsgh9979@naver.com>
2023-04-18 10:17:48 +0900

    Feat: Board하위 Event 추가

---
+++ src/main/java/shop/donutmarket/donut/domain/board/service/EventService.java
@@ -0,0 +1,13 @@
+package shop.donutmarket.donut.domain.board.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import shop.donutmarket.donut.domain.board.repository.EventRepository;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+    
+    private final EventRepository eventRepository;
+}
