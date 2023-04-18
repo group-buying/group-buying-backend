9218998b5cd37fb7c79dcd87af66defce2b35c8e
K-Minho <alsgh9979@naver.com>
2023-04-18 09:39:58 +0900

    Feat: Board 추가

---
+++ src/main/java/shop/donutmarket/donut/domain/board/repository/BoardRepository.java
@@ -0,0 +1,11 @@
+package shop.donutmarket.donut.domain.board.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import shop.donutmarket.donut.domain.board.model.Board;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long>{
+
+}
