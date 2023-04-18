bd385e52d25dc257053f86572b4baf21be47ebee
Hoseok Seong <shsshs0125@naver.com>
2023-04-18 10:31:51 +0900

    Feat: MyDateUtils, MyExceptionAdvice 생성

---
+++ src/main/java/shop/donutmarket/donut/global/exception/MyExceptionAdvice.java
@@ -0,0 +1,16 @@
+package shop.donutmarket.donut.global.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import shop.donutmarket.donut.global.dto.ResponseDTO;
+
+@RestControllerAdvice
+public class MyExceptionAdvice {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> ex(Exception e) {
+        ResponseDTO<?> responseDTO = new ResponseDTO<>().fail(500, "오류", HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
