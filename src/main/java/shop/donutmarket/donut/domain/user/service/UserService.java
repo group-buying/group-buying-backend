fa43d46dab42c010d8167299051538475c5576ae
Hoseok Seong <shsshs0125@naver.com>
2023-04-19 18:01:50 +0900

    Feat: 회원가입, 로그인 기능 구현

--- src/main/java/shop/donutmarket/donut/domain/user/service/UserService.java
+++ src/main/java/shop/donutmarket/donut/domain/user/service/UserService.java
@@ -1,14 +1,54 @@
 package shop.donutmarket.donut.domain.user.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import shop.donutmarket.donut.domain.user.dto.UserRequest;
+import shop.donutmarket.donut.domain.user.dto.UserResponse;
+import shop.donutmarket.donut.domain.user.model.User;
 import shop.donutmarket.donut.domain.user.repository.UserRepository;
+import shop.donutmarket.donut.global.jwt.MyJwtProvider;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    /*
+     * 1. 트랜젝션 관리
+     * 2. 영속성 객체 변경감지
+     * 3. RequestDTO 요청받기
+     * 4. 비즈니스 로직 처리하기
+     * 5. ResponseDTO 응답하기
+     */
+
+    @Transactional
+    public UserResponse.JoinDTO 회원가입(UserRequest.JoinDTO joinDTO) {
+        String rawPassword = joinDTO.getPassword();
+        String encPassword = passwordEncoder.encode(rawPassword); // 60Byte
+        joinDTO.setPassword(encPassword);
+        User userPS = userRepository.save(joinDTO.toEntity());
+        return new UserResponse.JoinDTO(userPS);
+    }
+
+    public String 로그인(UserRequest.LoginDTO loginDTO) {
+        Optional<User> userOP = userRepository.findByEmail(loginDTO.getEmail());
+        if (userOP.isPresent()) {
+            User userPS = userOP.get();
+            if (passwordEncoder.matches(loginDTO.getPassword(), userPS.getPassword())) {
+                String jwt = MyJwtProvider.create(userPS);
+                return jwt;
+            }
+            throw new RuntimeException("패스워드 유효성 실패");
+        } else {
+            throw new RuntimeException("이메일 유효성 실패");
+        }
+    }
     
 }
