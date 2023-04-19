24358451a16bc090e489e034f8dcc1ee4f65b9f2
Hoseok Seong <shsshs0125@naver.com>
2023-04-19 14:33:03 +0900

    Feat: Security UserDetails, UserDetailsService 완료

--- src/main/java/shop/donutmarket/donut/domain/user/repository/UserRepository.java
+++ src/main/java/shop/donutmarket/donut/domain/user/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package shop.donutmarket.donut.domain.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import shop.donutmarket.donut.domain.user.model.User;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, Long>{
-    
+    @Query("select u from User u where u.email = :email")
+    Optional<User> findByEmail(@Param("email") String email);
 }
---
+++ src/main/java/shop/donutmarket/donut/global/auth/MyUserDetailsService.java
@@ -0,0 +1,30 @@
+package shop.donutmarket.donut.global.auth;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import shop.donutmarket.donut.domain.user.model.User;
+import shop.donutmarket.donut.domain.user.repository.UserRepository;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    // /login + POST + FormUrlEncoded + username, password
+    // Authentication 객체 만들어짐
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> userOP = userRepository.findByEmail(email);
+        if (userOP.isPresent()) {
+            return new MyUserDetails(userOP.get());
+        } else {
+            return null;
+        }
+    }
+}
