e85979c28085e69e9256df8afb02e9d925ee0a5d
K-Minho <alsgh9979@naver.com>
2023-04-19 12:17:44 +0900

    Feat: Update, BoardResp, TagDelete생성, Debug: Tag List화

--- src/main/java/shop/donutmarket/donut/domain/board/dto/BoardResp.java
+++ src/main/java/shop/donutmarket/donut/domain/board/dto/BoardResp.java
@@ -1,6 +1,22 @@
 package shop.donutmarket.donut.domain.board.dto;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import shop.donutmarket.donut.domain.board.model.Board;
+import shop.donutmarket.donut.domain.board.model.Tag;
+
+
 public class BoardResp {
     
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class BoardDetailRespDTO {
+        private Board board;
+        private List<Tag> tagList;
+    }
 }
 
--- src/main/java/shop/donutmarket/donut/domain/board/repository/TagRepository.java
+++ src/main/java/shop/donutmarket/donut/domain/board/repository/TagRepository.java
@@ -3,6 +3,7 @@ package shop.donutmarket.donut.domain.board.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -10,7 +11,11 @@ import shop.donutmarket.donut.domain.board.model.Tag;
 
 public interface TagRepository extends JpaRepository<Tag, Long>{
 
-    @Query("select * from tag where board_id = :id")
-    List<Tag> findAllByBoardId(@Param("id") Long id);
+    @Query("select from tag where board_id = :BoardId")
+    List<Tag> findAllByBoardId(@Param("BoardId") Long BoardId);
+
+    @Modifying
+    @Query("delete from tag where board_id = :BoardId")
+    void deleteAllByBoardId(@Param("BoardId") Long BoardId);
     
 }
