3396b444ba3dea2631af254339f100d14f3451a2
K-Minho <alsgh9979@naver.com>
2023-04-19 10:18:16 +0900

    Feat: 공고 작성 Service, Base64Decoder 생성

--- src/main/java/shop/donutmarket/donut/domain/board/controller/BoardController.java
+++ src/main/java/shop/donutmarket/donut/domain/board/controller/BoardController.java
@@ -3,12 +3,33 @@ package shop.donutmarket.donut.domain.board.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import shop.donutmarket.donut.domain.board.dto.BoardReq.BoardSaveReqDTO;
 import shop.donutmarket.donut.domain.board.service.BoardService;
+import shop.donutmarket.donut.domain.user.model.User;
+import shop.donutmarket.donut.global.dto.ResponseDTO;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 @RestController
 @RequiredArgsConstructor
 public class BoardController {
     
     private final BoardService boardService;
+
+    @PostMapping("/boards")
+    public @ResponseBody ResponseEntity<?> save(@RequestBody BoardSaveReqDTO boardSaveReqDTO, BindingResult bindingResult) {
+        
+        boardService.공고작성(boardSaveReqDTO, user);
+        return new ResponseEntity<>(new ResponseDTO<>(), HttpStatus.valueOf(201));
+    }
     
 }
--- src/main/java/shop/donutmarket/donut/domain/board/service/BoardService.java
+++ src/main/java/shop/donutmarket/donut/domain/board/service/BoardService.java
@@ -1,14 +1,61 @@
 package shop.donutmarket.donut.domain.board.service;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import shop.donutmarket.donut.domain.board.dto.BoardReq.BoardSaveReqDTO;
+import shop.donutmarket.donut.domain.board.model.Board;
+import shop.donutmarket.donut.domain.board.model.Event;
+import shop.donutmarket.donut.domain.board.model.Tag;
 import shop.donutmarket.donut.domain.board.repository.BoardRepository;
+import shop.donutmarket.donut.domain.board.repository.EventRepository;
+import shop.donutmarket.donut.domain.board.repository.TagRepository;
+import shop.donutmarket.donut.domain.user.model.User;
+import shop.donutmarket.donut.global.util.MyBase64Decoder;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
 
     private final BoardRepository boardRepository;
+    private final EventRepository eventRepository;
+    private final TagRepository tagRepository;
+
+    public void 공고작성(BoardSaveReqDTO boardSaveReqDTO, User user) {
+
+        // event 먼저 save
+        Event event = Event.builder().latitude(boardSaveReqDTO.getLatitude())
+        .longtitude(boardSaveReqDTO.getLongtitude()).qty(boardSaveReqDTO.getQty())
+        .paymentType(boardSaveReqDTO.getPaymentType()).startAt(LocalDateTime.now())
+        .endAt(boardSaveReqDTO.getEndAt()).price(boardSaveReqDTO.getPrice()).build();
+        
+        event = eventRepository.save(event);
+        
+        // image base64화
+        String image = null;
+        try {
+            image = MyBase64Decoder.saveImage(boardSaveReqDTO.getImg());
+        } catch (IOException e) {
+            // Exception 처리 필요
+        }
+
+        // board save
+        Board board = Board.builder().category(boardSaveReqDTO.getCategory())
+        .title(boardSaveReqDTO.getTitle()).organizer(user).event(event).img(image)
+        .content(boardSaveReqDTO.getContent()).statusCode(boardSaveReqDTO.getStatusCode())
+        .state(boardSaveReqDTO.getState()).city(boardSaveReqDTO.getCity())
+        .town(boardSaveReqDTO.getTown()).createdAt(LocalDateTime.now()).build();
+
+        board = boardRepository.save(board);
+
+        // tag save
+        Tag tag = Tag.builder().board(board).comment(boardSaveReqDTO.getComment())
+        .createdAt(LocalDateTime.now()).build();
+
+        tagRepository.save(tag);
+    }
 
 }
