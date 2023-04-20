69778531552e75894f5af48c229d0b2df6e3f1fc
K-Minho <alsgh9979@naver.com>
2023-04-20 10:10:47 +0900

    Feat: 내참가목록 생성

--- src/main/java/shop/donutmarket/donut/domain/participant/service/ParticipantService.java
+++ src/main/java/shop/donutmarket/donut/domain/participant/service/ParticipantService.java
@@ -1,8 +1,11 @@
 package shop.donutmarket.donut.domain.participant.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import shop.donutmarket.donut.domain.participant.model.Participant;
 import shop.donutmarket.donut.domain.participant.repository.ParticipantRepository;
 
 @Service
@@ -10,4 +13,10 @@ import shop.donutmarket.donut.domain.participant.repository.ParticipantRepositor
 public class ParticipantService {
     
     private final ParticipantRepository participantRepository;
+
+    public List<Participant> 내참가목록(Long id) {
+        List<Participant> myParticipants = participantRepository.findAllByUserId(id);
+        return myParticipants;
+    }
+
 }
