9d3676bb331208d03962c1bf7e5d16051da37e85
K-Minho <alsgh9979@naver.com>
2023-04-20 10:12:16 +0900

    Feat: 참가하기 생성

--- src/main/java/shop/donutmarket/donut/domain/participant/controller/ParticipantController.java
+++ src/main/java/shop/donutmarket/donut/domain/participant/controller/ParticipantController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import shop.donutmarket.donut.domain.participant.dto.ParticipantReq.ParticipantSaveReqDTO;
+import shop.donutmarket.donut.domain.participant.dto.ParticipantResp.ParticipantSaveRespDTO;
 import shop.donutmarket.donut.domain.participant.model.Participant;
 import shop.donutmarket.donut.domain.participant.service.ParticipantService;
 import shop.donutmarket.donut.global.dto.ResponseDTO;
@@ -24,6 +27,13 @@ public class ParticipantController {
         List<Participant> participantList = participantService.내참가목록(id);
 
         return new ResponseEntity<>(new ResponseDTO<>().data(participantList), HttpStatus.OK);
+    }
+
+    @PostMapping("/participants")
+    public @ResponseBody ResponseEntity<?> save(ParticipantSaveReqDTO participantSaveReqDTO) {
+        ParticipantSaveRespDTO saveRespDTO = participantService.참가하기(participantSaveReqDTO);
+
+        return new ResponseEntity<>(new ResponseDTO<>().data(saveRespDTO), HttpStatus.CREATED);
     }
 
 }
