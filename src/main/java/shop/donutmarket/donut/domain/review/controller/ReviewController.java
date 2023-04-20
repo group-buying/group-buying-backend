fbf2ebd1126619ae43452c9a7e80cabd29d48de0
K-Minho <alsgh9979@naver.com>
2023-04-20 12:27:39 +0900

    Feat: 내리뷰목록 생성

--- src/main/java/shop/donutmarket/donut/domain/review/controller/ReviewController.java
+++ src/main/java/shop/donutmarket/donut/domain/review/controller/ReviewController.java
@@ -1,13 +1,28 @@
 package shop.donutmarket.donut.domain.review.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import shop.donutmarket.donut.domain.review.model.Review;
 import shop.donutmarket.donut.domain.review.service.ReviewService;
+import shop.donutmarket.donut.global.dto.ResponseDTO;
 
 @RestController
 @RequiredArgsConstructor
 public class ReviewController {
     
     private final ReviewService reviewService;
+
+    @GetMapping("/reviews")
+    public @ResponseBody ResponseEntity<?> myReviews(Long id) {
+        List<Review> reviewList = reviewService.내리뷰목록(id);
+        return new ResponseEntity<>(new ResponseDTO<>().data(reviewList), HttpStatus.OK);
+    }
+
 }
--- src/main/java/shop/donutmarket/donut/domain/review/repository/RateRepository.java
+++ src/main/java/shop/donutmarket/donut/domain/review/repository/RateRepository.java
@@ -1,11 +1,11 @@
 package shop.donutmarket.donut.domain.review.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import shop.donutmarket.donut.domain.review.model.Rate;
 
 
 public interface RateRepository extends JpaRepository<Rate, Long>{
+
     
 }
