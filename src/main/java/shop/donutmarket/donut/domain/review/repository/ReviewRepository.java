5e3345875172b2086d662f98536ebb7416704ff7
K-Minho <alsgh9979@naver.com>
2023-04-20 12:27:39 +0900

    Feat: 내리뷰목록 생성

--- src/main/java/shop/donutmarket/donut/domain/review/repository/ReviewRepository.java
+++ src/main/java/shop/donutmarket/donut/domain/review/repository/ReviewRepository.java
@@ -1,10 +1,16 @@
 package shop.donutmarket.donut.domain.review.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import shop.donutmarket.donut.domain.review.model.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>{
+
+    @Query("select from review where reviewer_id = :userId")
+    List<Review> findAllByUserId(@Param("userId") Long userId);
     
 }
--- src/main/java/shop/donutmarket/donut/domain/review/service/ReviewService.java
+++ src/main/java/shop/donutmarket/donut/domain/review/service/ReviewService.java
@@ -1,8 +1,11 @@
 package shop.donutmarket.donut.domain.review.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import shop.donutmarket.donut.domain.review.model.Review;
 import shop.donutmarket.donut.domain.review.repository.ReviewRepository;
 
 @Service
@@ -10,4 +13,11 @@ import shop.donutmarket.donut.domain.review.repository.ReviewRepository;
 public class ReviewService {
     
     private final ReviewRepository reviewRepository;
+
+    public List<Review> 내리뷰목록(Long id) {
+        List<Review> myReviews = reviewRepository.findAllByUserId(id);
+        return myReviews;
+    }
+
+
 }
