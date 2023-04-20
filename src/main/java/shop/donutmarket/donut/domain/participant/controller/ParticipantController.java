8cd9001283aa8a1a454cb4da30faf8c3a5f37521
K-Minho <alsgh9979@naver.com>
2023-04-20 10:10:47 +0900

    Feat: 내참가목록 생성

--- src/main/java/shop/donutmarket/donut/domain/participant/controller/ParticipantController.java
+++ src/main/java/shop/donutmarket/donut/domain/participant/controller/ParticipantController.java
@@ -1,13 +1,29 @@
 package shop.donutmarket.donut.domain.participant.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import shop.donutmarket.donut.domain.participant.model.Participant;
 import shop.donutmarket.donut.domain.participant.service.ParticipantService;
+import shop.donutmarket.donut.global.dto.ResponseDTO;
 
 @RestController
 @RequiredArgsConstructor
 public class ParticipantController {
     
     private final ParticipantService participantService;
+
+    @GetMapping("/participants")
+    public @ResponseBody ResponseEntity<?> myParticipants(Long id) {
+        List<Participant> participantList = participantService.내참가목록(id);
+
+        return new ResponseEntity<>(new ResponseDTO<>().data(participantList), HttpStatus.OK);
+    }
+
 }
--- src/main/java/shop/donutmarket/donut/domain/participant/repository/ParticipantRepository.java
+++ src/main/java/shop/donutmarket/donut/domain/participant/repository/ParticipantRepository.java
@@ -1,10 +1,16 @@
 package shop.donutmarket.donut.domain.participant.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import shop.donutmarket.donut.domain.participant.model.Participant;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Long>{
+
+    @Query("select from participant where user_id = :userId")
+    List<Participant> findAllByUserId(@Param("userId") Long userId);
     
 }
