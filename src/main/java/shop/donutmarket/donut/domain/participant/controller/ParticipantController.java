251eb95bf2b44caaea4e00e87e6b80d6524837a4
K-Minho <alsgh9979@naver.com>
2023-04-20 14:24:57 +0900

    Refactor: Vaild추가

--- src/main/java/shop/donutmarket/donut/domain/participant/controller/ParticipantController.java
+++ src/main/java/shop/donutmarket/donut/domain/participant/controller/ParticipantController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,35 +30,35 @@ public class ParticipantController {
     private final ParticipantService participantService;
 
     @GetMapping("/participants")
-    public @ResponseBody ResponseEntity<?> myParticipants(Long id) {
+    public @ResponseBody ResponseEntity<?> myParticipants(@RequestBody Long id) {
         List<Participant> participantList = participantService.내참가목록(id);
 
         return new ResponseEntity<>(new ResponseDTO<>().data(participantList), HttpStatus.OK);
     }
 
     @PostMapping("/participants")
-    public @ResponseBody ResponseEntity<?> save(ParticipantSaveReqDTO participantSaveReqDTO) {
+    public @ResponseBody ResponseEntity<?> save(@RequestBody @Vaild ParticipantSaveReqDTO participantSaveReqDTO) {
         ParticipantSaveRespDTO saveRespDTO = participantService.참가하기(participantSaveReqDTO);
 
         return new ResponseEntity<>(new ResponseDTO<>().data(saveRespDTO), HttpStatus.CREATED);
     }
 
     @PutMapping("/participants")
-    public @ResponseBody ResponseEntity<?> select(ParticipantSelectReqDTO participantSelectReqDTO) {
+    public @ResponseBody ResponseEntity<?> select(@RequestBody @Vaild ParticipantSelectReqDTO participantSelectReqDTO) {
         ParticipantSelectRespDTO selectRespDTO = participantService.채택하기(participantSelectReqDTO);
 
         return new ResponseEntity<>(new ResponseDTO<>().data(selectRespDTO), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/participants")
-    public @ResponseBody ResponseEntity<?> cancle(Long id) {
+    public @ResponseBody ResponseEntity<?> cancle(@RequestBody Long id) {
         ParticipantCancleRespDTO cancleRespDTO = participantService.취소하기(id);
         
         return new ResponseEntity<>(new ResponseDTO<>().data(cancleRespDTO), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/participants")
-    public @ResponseBody ResponseEntity<?> drop(Long id) {
+    public @ResponseBody ResponseEntity<?> drop(@RequestBody Long id) {
         ParticipantDropRespDTO dropRespDTO = participantService.강퇴하기(id);
 
         return new ResponseEntity<>(new ResponseDTO<>().data(dropRespDTO), HttpStatus.CREATED);
