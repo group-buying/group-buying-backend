9029aa5efb19d4e60163f378583687309d0408f0
K-Minho <alsgh9979@naver.com>
2023-04-21 14:46:36 +0900

    Debug: Participant내참가 목록보기 수정

--- src/main/java/shop/donutmarket/donut/domain/participant/dto/ParticipantResp.java
+++ src/main/java/shop/donutmarket/donut/domain/participant/dto/ParticipantResp.java
@@ -3,7 +3,9 @@ package shop.donutmarket.donut.domain.participant.dto;
 import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import shop.donutmarket.donut.domain.admin.model.StatusCode;
 import shop.donutmarket.donut.domain.board.model.Event;
@@ -11,6 +13,29 @@ import shop.donutmarket.donut.domain.user.model.User;
 
 public class ParticipantResp {
     
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class ParticipantListRespDTO {
+        private Event event;
+        private User user;
+        private int qty;
+        private LocalDateTime limitTime;
+        private StatusCode statusCode;
+        private LocalDateTime createdAt;
+
+        @Builder
+        public ParticipantListRespDTO(Event event, User user, int qty, LocalDateTime limitTime, StatusCode statusCode,
+                LocalDateTime createdAt) {
+            this.event = event;
+            this.user = user;
+            this.qty = qty;
+            this.limitTime = limitTime;
+            this.statusCode = statusCode;
+            this.createdAt = createdAt;
+        }
+    }
+
     @Getter
     @Setter
     @AllArgsConstructor
