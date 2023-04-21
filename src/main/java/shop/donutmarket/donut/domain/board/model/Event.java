83a23dbb9fee5d67edf7e6c1353aeefcee5e47b9
K-Minho <alsgh9979@naver.com>
2023-04-21 14:01:27 +0900

    Refactor: boardImport 정리

--- src/main/java/shop/donutmarket/donut/domain/board/model/Event.java
+++ src/main/java/shop/donutmarket/donut/domain/board/model/Event.java
@@ -2,12 +2,15 @@ package shop.donutmarket.donut.domain.board.model;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import shop.donutmarket.donut.domain.admin.model.StatusCode;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
--- src/main/java/shop/donutmarket/donut/domain/board/repository/BoardRepository.java
+++ src/main/java/shop/donutmarket/donut/domain/board/repository/BoardRepository.java
@@ -1,7 +1,6 @@
 package shop.donutmarket.donut.domain.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import shop.donutmarket.donut.domain.board.model.Board;
 
