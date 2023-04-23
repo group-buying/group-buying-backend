07d92f89a4a027bf9b561fc7f330e4074f35bc8e
Hoseok Seong <shsshs0125@naver.com>
2023-04-24 00:45:13 +0900

    Refactor: MyExceptionAdvice 리팩토링

--- src/main/java/shop/donutmarket/donut/global/dto/ResponseDto.java
+++ src/main/java/shop/donutmarket/donut/global/dto/ResponseDto.java
@@ -2,26 +2,27 @@ package shop.donutmarket.donut.global.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @Setter
-public class ResponseDTO<T> {
+public class ResponseDto<T> {
     private Integer status;
     private String msg; // 제목
     private T data; // 상세내용
 
-    public ResponseDTO() {
+    public ResponseDto() {
         this.status = 200;
         this.msg = "성공";
     }
 
-    public ResponseDTO<?> data(T data) {
+    public ResponseDto<?> data(T data) {
         this.data = data;
         return this;
     }
 
-    public ResponseDTO<?> fail(Integer status, String msg, T data) {
-        this.status = status;
+    public ResponseDto<?> fail(HttpStatus status, String msg, T data) {
+        this.status = status.value();
         this.msg = msg;
         this.data = data;
         return this;
---
+++ src/main/java/shop/donutmarket/donut/global/exception/Exception400.java
@@ -0,0 +1,23 @@
+package shop.donutmarket.donut.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import shop.donutmarket.donut.global.dto.ResponseDto;
+
+// 유효성 실패
+@Getter
+public class Exception400 extends RuntimeException{
+    public Exception400(String message) {
+        super(message);
+    }
+
+    public ResponseDto<?> body(){
+        ResponseDto<String> responseDto = new ResponseDto<>();
+        responseDto.fail(HttpStatus.BAD_REQUEST, "badRequest", getMessage());
+        return responseDto;
+    }
+
+    public HttpStatus status(){
+        return HttpStatus.BAD_REQUEST;
+    }
+}
