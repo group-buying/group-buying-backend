8b1a3ace13ca886d0df01c0fbfd651da4131f684
Hoseok Seong <shsshs0125@naver.com>
2023-04-23 23:27:30 +0900

    Feat: DTO 반환 완료

--- src/main/java/shop/donutmarket/donut/domain/myPage/controller/MyPageController.java
+++ src/main/java/shop/donutmarket/donut/domain/myPage/controller/MyPageController.java
@@ -4,20 +4,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import shop.donutmarket.donut.domain.myPage.dto.MyPageResp;
 import shop.donutmarket.donut.domain.myPage.service.MyPageService;
 import shop.donutmarket.donut.global.auth.MyUserDetails;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/mypage")
 public class MyPageController {
 
     private final MyPageService myPageService;
 
-    @GetMapping()
+    @GetMapping("/board")
     public ResponseEntity<?> myBoard(@AuthenticationPrincipal MyUserDetails myUserDetails) {
-        myPageService.나의게시글보기(myUserDetails.getUser().getId());
-        return ResponseEntity.ok().body();
+        List<MyPageResp.MyBoardDTO> myBoardDTOS = myPageService.나의게시글보기(myUserDetails.getUser().getId());
+        return ResponseEntity.ok().body(myBoardDTOS);
     }
-
 }
--- src/main/java/shop/donutmarket/donut/domain/myPage/dto/MyPageResp.java
+++ src/main/java/shop/donutmarket/donut/domain/myPage/dto/MyPageResp.java
@@ -1,4 +1,14 @@
 package shop.donutmarket.donut.domain.myPage.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import shop.donutmarket.donut.domain.board.model.Board;
+
 public class MyPageResp {
+
+    @Getter
+    @Setter
+    public static class MyBoardDTO {
+        private Board board;
+    }
 }
