05e313afe7311f434355b2559b21e4bc37d9f578
K-Minho <alsgh9979@naver.com>
2023-04-24 18:33:23 +0900

    Feat: 채팅 초대하기

--- src/main/java/shop/donutmarket/donut/domain/chat/controller/ChatController.java
+++ src/main/java/shop/donutmarket/donut/domain/chat/controller/ChatController.java
@@ -5,10 +5,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import shop.donutmarket.donut.domain.chat.dto.ChatReq.ChatInviteReqDTO;
 import shop.donutmarket.donut.domain.chat.dto.ChatResp.MyChatListRespDTO;
 import shop.donutmarket.donut.domain.chat.service.ChatterListService;
 import shop.donutmarket.donut.global.auth.MyUserDetails;
@@ -30,6 +35,12 @@ public class ChatController {
     @GetMapping("/{id}")
     public ResponseEntity<?> room(@PathVariable Long id, @AuthenticationPrincipal MyUserDetails myUserDetails) {
         // 임시 저장
+        return new ResponseEntity<>(new ResponseDTO<>(), HttpStatus.OK);
+    }
+
+    @PostMapping("/invite")
+    public ResponseEntity<?> invite(@RequestBody @Valid ChatInviteReqDTO chatInviteReqDTO, @AuthenticationPrincipal MyUserDetails myUserDetails) {
+        chatterListService.초대하기(chatInviteReqDTO);
         return new ResponseEntity<>(new ResponseDTO<>(), HttpStatus.OK);
     }
 
