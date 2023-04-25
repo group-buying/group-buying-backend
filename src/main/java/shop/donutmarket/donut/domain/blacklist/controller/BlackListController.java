161b67f70634ddf9350764b48c79ab34b7f5050b
Hoseok Seong <shsshs0125@naver.com>
2023-04-25 15:04:57 +0900

    Feat: blacklist 추가 기능 구현

--- src/main/java/shop/donutmarket/donut/domain/blacklist/controller/BlackListController.java
+++ src/main/java/shop/donutmarket/donut/domain/blacklist/controller/BlackListController.java
@@ -1,9 +1,17 @@
 package shop.donutmarket.donut.domain.blacklist.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import shop.donutmarket.donut.domain.blacklist.dto.BlacklistReq;
+import shop.donutmarket.donut.domain.blacklist.dto.BlacklistResp;
 import shop.donutmarket.donut.domain.blacklist.service.BlackListService;
+import shop.donutmarket.donut.global.auth.MyUserDetails;
 
 
 @RestController
@@ -11,4 +19,11 @@ import shop.donutmarket.donut.domain.blacklist.service.BlackListService;
 public class BlackListController {
     
     private final BlackListService blackListService;
+
+    @PostMapping("/blacklists/{id}")
+    public ResponseEntity<?> insert(@PathVariable Long id,
+                                    @AuthenticationPrincipal MyUserDetails myUserDetails) {
+        BlacklistResp.select resp = blackListService.블랙리스트추가(myUserDetails.getUser().getId(), id);
+        return ResponseEntity.ok().body(resp);
+    }
 }
--- src/main/java/shop/donutmarket/donut/domain/blacklist/service/BlackListService.java
+++ src/main/java/shop/donutmarket/donut/domain/blacklist/service/BlackListService.java
@@ -3,12 +3,42 @@ package shop.donutmarket.donut.domain.blacklist.service;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import shop.donutmarket.donut.domain.blacklist.dto.BlacklistReq;
+import shop.donutmarket.donut.domain.blacklist.dto.BlacklistResp;
+import shop.donutmarket.donut.domain.blacklist.model.Blacklist;
 import shop.donutmarket.donut.domain.blacklist.repository.BlackListRepository;
+import shop.donutmarket.donut.domain.user.model.User;
+import shop.donutmarket.donut.domain.user.repository.UserRepository;
+
+import java.util.Optional;
 
 
 @Service
 @RequiredArgsConstructor
 public class BlackListService {
-    
+
     private final BlackListRepository blackListRepository;
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public BlacklistResp.select 블랙리스트추가(Long userId, Long blockedUserId) {
+        try {
+            Optional<User> userOP = userRepository.findById(userId);
+            Optional<User> blockedUserOP = userRepository.findById(blockedUserId);
+
+            if (userOP.isPresent() && blockedUserOP.isPresent()) {
+                User userPS = userOP.get();
+                User blockedUserPS = blockedUserOP.get();
+                Blacklist blacklist = Blacklist.builder().user(userPS).blockedUser(blockedUserPS).build();
+                Blacklist blacklistPS = blackListRepository.save(blacklist);
+                BlacklistResp.select resp = new BlacklistResp.select(blacklistPS);
+                return resp;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
