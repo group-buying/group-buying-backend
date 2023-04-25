a755fc65339b1c1517fc17589e1777923d3a91a1
K-Minho <alsgh9979@naver.com>
2023-04-25 14:04:13 +0900

    Feat: 내지역 디폴트 설정 추가

--- src/main/java/shop/donutmarket/donut/domain/myLocation/repository/MyLocationRepository.java
+++ src/main/java/shop/donutmarket/donut/domain/myLocation/repository/MyLocationRepository.java
@@ -1,10 +1,15 @@
 package shop.donutmarket.donut.domain.myLocation.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import shop.donutmarket.donut.domain.myLocation.model.MyLocation;
 
 public interface MyLocationRepository extends JpaRepository<MyLocation, Long>{
     
+    @Query("select m from MyLocation m where m.user.id = :userId")
+    Optional<MyLocation> findByUserId(@Param("userId") Long userId);
 }
