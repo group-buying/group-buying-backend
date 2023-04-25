26916f367829201cd03ae417adf8362a7393f6bc
K-Minho <alsgh9979@naver.com>
2023-04-25 11:43:05 +0900

    Feat: 관심공고 등록 추가

--- src/main/java/shop/donutmarket/donut/domain/wishlist/repository/WishlistRepository.java
+++ src/main/java/shop/donutmarket/donut/domain/wishlist/repository/WishlistRepository.java
@@ -1,10 +1,15 @@
 package shop.donutmarket.donut.domain.wishlist.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import shop.donutmarket.donut.domain.wishlist.model.Wishlist;
 
 public interface WishlistRepository extends JpaRepository<Wishlist, Long>{
     
+    @Query("select w from Wishlist w where w.user.id = :userId")
+    List<Wishlist> findAllByUserId(@Param("userId") Long userId);
 }
--- src/main/java/shop/donutmarket/donut/domain/wishlist/service/WishlistService.java
+++ src/main/java/shop/donutmarket/donut/domain/wishlist/service/WishlistService.java
@@ -1,14 +1,40 @@
 package shop.donutmarket.donut.domain.wishlist.service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import shop.donutmarket.donut.domain.board.model.Board;
+import shop.donutmarket.donut.domain.board.repository.BoardRepository;
+import shop.donutmarket.donut.domain.user.model.User;
+import shop.donutmarket.donut.domain.wishlist.dto.WishlistReq.WishListSaveReqDTO;
+import shop.donutmarket.donut.domain.wishlist.dto.WishlistResp.WishListSaveRespDTO;
+import shop.donutmarket.donut.domain.wishlist.model.Wishlist;
 import shop.donutmarket.donut.domain.wishlist.repository.WishlistRepository;
+import shop.donutmarket.donut.global.auth.MyUserDetails;
 
 @Service
 @RequiredArgsConstructor
 public class WishlistService {
     
     private final WishlistRepository wishlistRepository;
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public WishListSaveRespDTO 관심등록(WishListSaveReqDTO wishListSaveReqDTO,  @AuthenticationPrincipal MyUserDetails myUserDetails) {
+        User user = myUserDetails.getUser();
+        Optional<Board> boardOP = boardRepository.findByIdWithAll(wishListSaveReqDTO.getBoardId());
+        Board boardPS = boardOP.get();
+
+        Wishlist wishlist = Wishlist.builder().board(boardPS).user(user).createdAt(LocalDateTime.now()).build();
+        wishlistRepository.save(wishlist);
+
+        WishListSaveRespDTO saveRespDTO = new WishListSaveRespDTO(boardPS.getTitle(), boardPS.getState(), boardPS.getCity(), LocalDateTime.now());
+        return saveRespDTO;
+    }
 
 }
