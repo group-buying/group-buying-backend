e20bfecc8d84f39d8951dc1e79a05f24e9833c1a
K-Minho <alsgh9979@naver.com>
2023-04-25 11:43:05 +0900

    Feat: 관심공고 등록 추가

--- src/main/java/shop/donutmarket/donut/domain/board/repository/BoardRepository.java
+++ src/main/java/shop/donutmarket/donut/domain/board/repository/BoardRepository.java
@@ -17,4 +17,10 @@ public interface BoardRepository extends JpaRepository<Board, Long>{
 
     @Query("select b from Board b where b.organizer.id =:organizerId")
     List<MyPageResp.MyBoardDTO> findByOrganizerId(@Param("organizerId") Long organizerId);
+
+    @Query("select b from Board b join fetch b.event where b.id = :boardId")
+    Optional<Board> findByIdWithEvent(@Param("boardId") Long boardId);
+
+    @Query("select b from Board b join fetch b.category c join fetch b.event e join fetch b.organizer o join fetch o.rate join fetch o.statusCode join fetch b.statusCode where b.id = :boardId")
+    Optional<Board> findByIdWithAll(@Param("boardId") Long boardId);
 }
--- src/main/java/shop/donutmarket/donut/domain/wishlist/controller/WishlistController.java
+++ src/main/java/shop/donutmarket/donut/domain/wishlist/controller/WishlistController.java
@@ -1,13 +1,32 @@
 package shop.donutmarket.donut.domain.wishlist.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import shop.donutmarket.donut.domain.wishlist.dto.WishlistReq.WishListSaveReqDTO;
+import shop.donutmarket.donut.domain.wishlist.dto.WishlistResp.WishListSaveRespDTO;
 import shop.donutmarket.donut.domain.wishlist.service.WishlistService;
+import shop.donutmarket.donut.global.auth.MyUserDetails;
+import shop.donutmarket.donut.global.dto.ResponseDTO;
+
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/wishlist")
 public class WishlistController {
     
     private final WishlistService wishlistService;
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody WishListSaveReqDTO wishListSaveReqDTO, @AuthenticationPrincipal MyUserDetails myUserDetails) {
+        WishListSaveRespDTO saveRespDTO =  wishlistService.관심등록(wishListSaveReqDTO, myUserDetails);
+        return new ResponseEntity<>(new ResponseDTO<>().data(saveRespDTO), HttpStatus.CREATED);
+    }
+    
 }
