7dee330aa5d9f8641ba8b30f6027e54cd0010764
K-Minho <alsgh9979@naver.com>
2023-04-26 10:27:34 +0900

    Refactor: 내참가목록 보기 리팩토링

--- src/main/java/shop/donutmarket/donut/domain/participant/repository/ParticipantRepository.java
+++ src/main/java/shop/donutmarket/donut/domain/participant/repository/ParticipantRepository.java
@@ -1,7 +1,6 @@
 package shop.donutmarket.donut.domain.participant.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +12,8 @@ public interface ParticipantRepository extends JpaRepository<Participant, Long>{
 
     @Query("select p from Participant p where p.user.id = :userId")
     List<Participant> findAllByUserId(@Param("userId") Long userId);
+    
+    @Query("select p from Participant p join fetch p.event join fetch p.statusCode where p.user.id = :userId")
+    List<Participant> findAllByUserIdwithEvent(@Param("userId") Long userId);
+
 }
