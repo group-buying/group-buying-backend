79e57591b58dab1a0d80f9eb3341bb17b2bd2f08
Hoseok Seong <shsshs0125@naver.com>
2023-04-26 14:55:23 +0900

    Feat: Sentry 연동 완료

--- src/main/java/shop/donutmarket/donut/global/exception/MyExceptionAdvice.java
+++ src/main/java/shop/donutmarket/donut/global/exception/MyExceptionAdvice.java
@@ -1,5 +1,6 @@
 package shop.donutmarket.donut.global.exception;
 
+import io.sentry.Sentry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,18 +15,21 @@ public class MyExceptionAdvice {
     // badRequest
     @ExceptionHandler(Exception400.class)
     public ResponseEntity<?> badRequest(Exception400 e){
+        Sentry.captureException(e);
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     // unAuthorized
     @ExceptionHandler(Exception401.class)
     public ResponseEntity<?> unAuthorized(Exception401 e){
+        Sentry.captureException(e);
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     // forbidden
     @ExceptionHandler(Exception403.class)
     public ResponseEntity<?> forbidden(Exception403 e){
+        Sentry.captureException(e);
         return new ResponseEntity<>(e.body(), e.status());
     }
 
@@ -33,6 +37,7 @@ public class MyExceptionAdvice {
     // 자원을 못 찾은 경우
     @ExceptionHandler(Exception404.class)
     public ResponseEntity<?> notFound(Exception404 e){
+        Sentry.captureException(e);
         return new ResponseEntity<>(e.body(), e.status());
     }
 
@@ -40,6 +45,7 @@ public class MyExceptionAdvice {
     // 주소를 못 찾은 경우
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<?> notFound(NoHandlerFoundException e){
+        Sentry.captureException(e);
         ResponseDTO<String> responseDto = new ResponseDTO<>();
         responseDto.fail(HttpStatus.NOT_FOUND, "notFound", e.getMessage());
         return new ResponseEntity<>(responseDto, HttpStatus.NOT_FOUND);
@@ -48,12 +54,14 @@ public class MyExceptionAdvice {
     // ServerError
     @ExceptionHandler(Exception500.class)
     public ResponseEntity<?> serverError(Exception500 e){
+        Sentry.captureException(e);
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     // 나머지 모든 예외
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> serverError(Exception e){
+        Sentry.captureException(e);
         ResponseDTO<String> responseDto = new ResponseDTO<>();
         responseDto.fail(HttpStatus.INTERNAL_SERVER_ERROR, "unknownServerError", e.getMessage());
         return new ResponseEntity<>(responseDto, HttpStatus.INTERNAL_SERVER_ERROR);
