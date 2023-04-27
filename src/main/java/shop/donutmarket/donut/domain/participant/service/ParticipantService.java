dcf2611763037f46f7408d62c2cb88c48eaad419
Hoseok Seong <shsshs0125@naver.com>
2023-04-27 11:39:20 +0900

    Refactor: Participant Exception 처리 완료

--- src/main/java/shop/donutmarket/donut/domain/participant/service/ParticipantService.java
+++ src/main/java/shop/donutmarket/donut/domain/participant/service/ParticipantService.java
@@ -2,6 +2,7 @@ package shop.donutmarket.donut.domain.participant.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -26,132 +27,150 @@ import shop.donutmarket.donut.domain.participant.model.Participant;
 import shop.donutmarket.donut.domain.participant.repository.ParticipantRepository;
 import shop.donutmarket.donut.domain.user.model.User;
 import shop.donutmarket.donut.global.auth.MyUserDetails;
+import shop.donutmarket.donut.global.exception.Exception403;
+import shop.donutmarket.donut.global.exception.Exception404;
+import shop.donutmarket.donut.global.exception.Exception500;
 
 @Service
 @RequiredArgsConstructor
 public class ParticipantService {
-    
+
     private final ParticipantRepository participantRepository;
     private final BoardRepository boardRepository;
 
+    @Transactional(readOnly = true)
     public List<ParticipantListRespDTO> 내참가목록(@AuthenticationPrincipal MyUserDetails myUserDetails) {
-        User user = myUserDetails.getUser();
-        List<Participant> myParticipantsPS = participantRepository.findAllByUserIdwithEvent(user.getId());
-        List<ParticipantListRespDTO> participantList = new ArrayList<>();
-        
-        for (Participant participant : myParticipantsPS) {
-            Event eventPS = participant.getEvent();
-            StatusCode statusCodePS = participant.getStatusCode();
+        try {
+            User user = myUserDetails.getUser();
+            List<Participant> myParticipantsPS = participantRepository.findAllByUserIdwithEvent(user.getId());
+            List<ParticipantListRespDTO> participantList = new ArrayList<>();
 
-            ParticipantListRespDTO participantDTO = ParticipantListRespDTO.builder().event(eventPS).user(user)
-            .qty(participant.getQty()).limitTime(participant.getLimitTime()).statusCode(statusCodePS)
-            .createdAt(participant.getCreatedAt()).build();
+            for (Participant participant : myParticipantsPS) {
+                Event eventPS = participant.getEvent();
+                StatusCode statusCodePS = participant.getStatusCode();
 
-            participantList.add(participantDTO);
+                ParticipantListRespDTO participantDTO = ParticipantListRespDTO.builder().event(eventPS).user(user)
+                        .qty(participant.getQty()).limitTime(participant.getLimitTime()).statusCode(statusCodePS)
+                        .createdAt(participant.getCreatedAt()).build();
+
+                participantList.add(participantDTO);
+            }
+            return participantList;
+        } catch (Exception e) {
+            throw new Exception500("내 참가목록 보기 실패 : " + e.getMessage());
         }
-        return participantList;
     }
 
     @Transactional
     public ParticipantSaveRespDTO 참가하기(ParticipantSaveReqDTO participantSaveReqDTO, @AuthenticationPrincipal MyUserDetails myUserDetails) {
-        
-        // 이미 참가한 내용 재참가 막는 예외 처리 필요
 
+
+        try {
         Participant participant = participantSaveReqDTO.toEntity(myUserDetails.getUser());
         participantRepository.save(participant);
 
         Event eventPS = participantSaveReqDTO.getEvent();
 
         ParticipantSaveRespDTO saveRespDTO = new ParticipantSaveRespDTO(
-            eventPS, myUserDetails.getUser(), participant.getQty(),
-            participant.getLimitTime(), participant.getStatusCode(), participant.getCreatedAt());
+                eventPS, myUserDetails.getUser(), participant.getQty(),
+                participant.getLimitTime(), participant.getStatusCode(), participant.getCreatedAt());
 
         return saveRespDTO;
+        } catch (Exception e) {
+            throw new Exception500("이벤트 참가하기 실패 : " + e.getMessage());
+        }
+        // 이미 참가한 내용 재참가 막는 예외 처리 필요
+
     }
 
     @Transactional
     public ParticipantSelectRespDTO 채택하기(ParticipantSelectReqDTO participantSelectReqDTO, @AuthenticationPrincipal MyUserDetails myUserDetails) {
-
-        Optional<Participant> particiOP = participantRepository.findByIdwithEvent(participantSelectReqDTO.getId());
-        if (!particiOP.isPresent()) {
-            // 없을때 예외처리
+        Optional<Participant> participantOP = participantRepository.findByIdwithEvent(participantSelectReqDTO.getId());
+        if (participantOP.isEmpty()) {
+            throw new Exception404("존재하지 않는 참가자입니다");
         }
-        Participant particiPS = particiOP.get();
 
-        Optional<Board> boardOP = boardRepository.findByEventId(particiPS.getEvent().getId());
+        Participant participantPS = participantOP.get();
+
+        Optional<Board> boardOP = boardRepository.findByEventId(participantPS.getEvent().getId());
         Board boardPS = boardOP.get();
         Long organizerId = boardPS.getOrganizer().getId();
 
-        if(!(organizerId == myUserDetails.getUser().getId())){
-            // 권한없을때 처리
+        if (!(Objects.equals(organizerId, myUserDetails.getUser().getId()))) {
+            throw new Exception403("참가자를 채택할 권한이 없습니다");
         }
-        particiPS.selected();
 
-        Event eventPS = particiPS.getEvent();
-        User userPS = particiPS.getUser();
+        try {
+            participantPS.selected();
+            Event eventPS = participantPS.getEvent();
+            User userPS = participantPS.getUser();
 
-        User user = User.builder().id(userPS.getId()).name(userPS.getName())
-        .profile(userPS.getProfile()).rate(userPS.getRate()).build();
+            User user = User.builder().id(userPS.getId()).name(userPS.getName())
+                    .profile(userPS.getProfile()).rate(userPS.getRate()).build();
 
-        ParticipantSelectRespDTO selectRespDTO = new ParticipantSelectRespDTO(
-            particiPS.getId(), eventPS, user, "채택함");
+            ParticipantSelectRespDTO selectRespDTO = new ParticipantSelectRespDTO(
+                    participantPS.getId(), eventPS, user, "채택함");
 
-        return selectRespDTO;
+            return selectRespDTO;
+        } catch (Exception e) {
+            throw new Exception500("참가자 채택하기 실패 : " + e.getMessage());
+        }
     }
 
     @Transactional
     public ParticipantCancleRespDTO 취소하기(ParticipantCancelReqDTO participantCancelReqDTO, @AuthenticationPrincipal MyUserDetails myUserDetails) {
-
-        Optional<Participant> particiOP = participantRepository.findByIdwithEvent(participantCancelReqDTO.getId());
-        if (!particiOP.isPresent()) {
-            // 없을때 예외처리
+        Optional<Participant> participantOP = participantRepository.findByIdwithEvent(participantCancelReqDTO.getId());
+        if (participantOP.isEmpty()) {
+            throw new Exception404("존재하지 않는 참가자입니다");
         }
-        Participant particiPS = particiOP.get();
-        
-        if(!(particiPS.getUser().getId() == myUserDetails.getUser().getId())){
-            // 인증 없을때 예외처리
+        Participant participantPS = participantOP.get();
+
+        if (!(Objects.equals(participantPS.getUser().getId(), myUserDetails.getUser().getId()))) {
+            throw new Exception403("참가자를 취소할 권한이 없습니다");
         }
 
-        particiPS.canceled();
+        try {
+            participantPS.canceled();
+            Event eventPS = participantPS.getEvent();
 
-        Event eventPS = particiPS.getEvent();
-        
-        ParticipantCancleRespDTO cancleRespDTO = new ParticipantCancleRespDTO(
-            particiPS.getId(), eventPS, myUserDetails.getUser(), "취소함");
-            
-        return cancleRespDTO;
+            ParticipantCancleRespDTO cancleRespDTO = new ParticipantCancleRespDTO(
+                    participantPS.getId(), eventPS, myUserDetails.getUser(), "취소함");
+
+            return cancleRespDTO;
+        } catch (Exception e) {
+            throw new Exception500("참가자 채택 취소하기 실패 : " + e.getMessage());
+        }
     }
 
     @Transactional
     public ParticipantDropRespDTO 강퇴하기(ParticipantDropReqDTO participantDropReqDTO, @AuthenticationPrincipal MyUserDetails myUserDetails) {
-        
-        Optional<Participant> particiOP = participantRepository.findByIdwithEvent(participantDropReqDTO.getId());
-        if (!particiOP.isPresent()) {
-            // 없을때 예외처리
+        Optional<Participant> participantOP = participantRepository.findByIdwithEvent(participantDropReqDTO.getId());
+        if (participantOP.isEmpty()) {
+            throw new Exception404("존재하지 않는 참가자입니다");
         }
-        Participant particiPS = particiOP.get();
-        
-        Optional<Board> boardOP = boardRepository.findByEventId(particiPS.getEvent().getId());
+        Participant participantPS = participantOP.get();
+
+        Optional<Board> boardOP = boardRepository.findByEventId(participantPS.getEvent().getId());
         Board boardPS = boardOP.get();
         Long organizerId = boardPS.getOrganizer().getId();
 
-        if(!(organizerId == myUserDetails.getUser().getId())){
-            // 권한없을때 처리
+        if (!(Objects.equals(organizerId, myUserDetails.getUser().getId()))) {
+            throw new Exception403("참가자를 강퇴할 권한이 없습니다");
         }
+        try {
+            participantPS.droped();
+            Event eventPS = participantPS.getEvent();
+            User userPS = participantPS.getUser();
 
-        particiPS.droped();
-        Event eventPS = particiPS.getEvent();
- 
-        User userPS = particiPS.getUser();
+            User user = User.builder().id(userPS.getId()).name(userPS.getName())
+                    .profile(userPS.getProfile()).rate(userPS.getRate()).build();
 
+            ParticipantDropRespDTO dropRespDTO = new ParticipantDropRespDTO(
+                    participantPS.getId(), eventPS, user, "강퇴함");
 
-        User user = User.builder().id(userPS.getId()).name(userPS.getName())
-        .profile(userPS.getProfile()).rate(userPS.getRate()).build();
-
-        ParticipantDropRespDTO dropRespDTO = new ParticipantDropRespDTO(
-            particiPS.getId(), eventPS, user, "강퇴함");
-            
-        return dropRespDTO;
+            return dropRespDTO;
+        } catch (Exception e) {
+            throw new Exception500("참가자 강퇴하기 실패 : " + e.getMessage());
+        }
     }
-
 }
