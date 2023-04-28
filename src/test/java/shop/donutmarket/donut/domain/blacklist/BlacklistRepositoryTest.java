793432fbda4f95ca8719e948f4773a9045a86118
Hoseok Seong <shsshs0125@naver.com>
2023-04-28 15:15:14 +0900

    Refactor: 환경에 맞는 yml 생성

--- src/test/java/shop/donutmarket/donut/domain/blacklist/BlacklistRepositoryTest.java
+++ src/test/java/shop/donutmarket/donut/domain/blacklist/BlacklistRepositoryTest.java
@@ -104,6 +104,6 @@ public class BlacklistRepositoryTest {
     }
 
     private void autoincrementReset() {
-        em.createNativeQuery("ALTER TABLE blackList ALTER COLUMN id RESTART WITH 1").executeUpdate();
+        em.createNativeQuery("ALTER TABLE blacklist ALTER COLUMN id RESTART WITH 1").executeUpdate();
     }
 }
