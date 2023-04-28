eeb949307b974491fe6f1e5be8fbcacb3ab12a30
Hoseok Seong <shsshs0125@naver.com>
2023-04-28 16:50:54 +0900

    Feat: RestDocs 설정 완료

--- src/test/java/shop/donutmarket/donut/integration/account/AccountControllerTest.java
+++ src/test/java/shop/donutmarket/donut/integration/account/AccountControllerTest.java
@@ -4,22 +4,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import shop.donutmarket.donut.core.MyRestDocs;
 import shop.donutmarket.donut.domain.user.repository.UserRepository;
 import shop.donutmarket.donut.global.dummy.DummyEntity;
 
 @DisplayName("회원 API")
 @AutoConfigureRestDocs(uriScheme = "http", uriHost = "localhost", uriPort = 8080)
 @ActiveProfiles("test")
-//@Sql("classpath:db/teardown.sql")
+@Sql("classpath:db/teardown.sql")
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-public class AccountControllerTest {
+public class AccountControllerTest extends MyRestDocs {
 
     private DummyEntity dummy = new DummyEntity();
 
