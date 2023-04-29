e2eb57b53e545a603274e571286b1a445b612745
Hoseok Seong <shsshs0125@naver.com>
2023-04-29 22:58:37 +0900

    Test: MyLocation 통합테스트 완료

--- src/main/java/shop/donutmarket/donut/domain/myLocation/dto/MyLocationReq.java
+++ src/main/java/shop/donutmarket/donut/domain/myLocation/dto/MyLocationReq.java
@@ -4,11 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 public class MyLocationReq {
     @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
+    @Setter
     public static class MyLocationSaveReqDTO {
         @NotBlank
         private String state;
---
+++ src/test/java/shop/donutmarket/donut/integration/myLocation/MyLocationControllerTest.java
@@ -0,0 +1,124 @@
+package shop.donutmarket.donut.integration.myLocation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import shop.donutmarket.donut.core.MyRestDocs;
+import shop.donutmarket.donut.domain.admin.repository.CategoryRepository;
+import shop.donutmarket.donut.domain.myCategory.repository.MyCategoryRepository;
+import shop.donutmarket.donut.domain.myLocation.dto.MyLocationReq;
+import shop.donutmarket.donut.domain.myLocation.model.MyLocation;
+import shop.donutmarket.donut.domain.myLocation.repository.MyLocationRepository;
+import shop.donutmarket.donut.domain.user.model.User;
+import shop.donutmarket.donut.domain.user.repository.UserRepository;
+import shop.donutmarket.donut.global.dummy.DummyEntity;
+
+import java.util.Optional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("나의 지역 API")
+@AutoConfigureRestDocs(uriScheme = "http", uriHost = "localhost", uriPort = 8080)
+@ActiveProfiles("test")
+@Sql("classpath:db/teardown.sql")
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class MyLocationControllerTest extends MyRestDocs {
+    private DummyEntity dummy = new DummyEntity();
+
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private ObjectMapper om;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private MyLocationRepository myLocationRepository;
+    @Autowired
+    private EntityManager em;
+
+    @BeforeEach
+    public void setUp() {
+        userRepository.save(dummy.newUser("ssar@naver.com", "쌀"));
+        userRepository.save(dummy.newUser("cos@naver.com", "쌀"));
+
+        // 내 지역 dummy 생성
+        Optional<User> userOP = userRepository.findById(1L);
+        User userPS = userOP.get();
+        MyLocation myLocation = MyLocation.builder().user(userPS).state("창원시").city("성산구").town("남양동").build();
+        myLocationRepository.save(myLocation);
+
+        em.clear();
+    }
+
+    @AfterEach
+    void clean() {
+        myLocationRepository.deleteAll();
+    }
+
+
+    @DisplayName("디폴트 지역")
+    @WithUserDetails(value = "ssar@naver.com", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @Test
+    public void default_test() throws Exception {
+        // given
+
+        // when
+        ResultActions resultActions = mvc
+                .perform(post("/myLocations/default"));
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // then
+        resultActions.andExpect(jsonPath("$.state").value("부산광역시"));
+        resultActions.andExpect(jsonPath("$.city").value("부산진구"));
+        resultActions.andExpect(jsonPath("$.town").value("부전동"));
+        resultActions.andExpect(status().isOk());
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @DisplayName("지역 변경")
+    @WithUserDetails(value = "ssar@naver.com", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @Test
+    public void update_test() throws Exception {
+        // given
+        MyLocationReq.MyLocationSaveReqDTO myLocationSaveReqDTO = new MyLocationReq.MyLocationSaveReqDTO();
+        myLocationSaveReqDTO.setState("부산광역시");
+        myLocationSaveReqDTO.setCity("해운대구");
+        myLocationSaveReqDTO.setTown("반송1동");
+        String requestBody = om.writeValueAsString(myLocationSaveReqDTO);
+
+        // when
+        ResultActions resultActions = mvc
+                .perform(put("/myLocations").content(requestBody).contentType(MediaType.APPLICATION_JSON));
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // then
+        resultActions.andExpect(jsonPath("$.state").value("부산광역시"));
+        resultActions.andExpect(jsonPath("$.city").value("해운대구"));
+        resultActions.andExpect(jsonPath("$.town").value("반송1동"));
+        resultActions.andExpect(status().isOk());
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+}
