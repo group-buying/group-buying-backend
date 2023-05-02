a15775685eb92afb6ca1d8222521ac45695e4375
Hoseok Seong <shsshs0125@naver.com>
2023-05-02 23:22:54 +0900

    Test: Main 통합테스트 완료

--- src/test/java/shop/donutmarket/donut/integration/main/MainControllerTest.java
+++ src/test/java/shop/donutmarket/donut/integration/main/MainControllerTest.java
@@ -17,11 +17,26 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import shop.donutmarket.donut.core.MyRestDocs;
+import shop.donutmarket.donut.domain.admin.model.Category;
+import shop.donutmarket.donut.domain.admin.repository.CategoryRepository;
+import shop.donutmarket.donut.domain.blacklist.model.Blacklist;
 import shop.donutmarket.donut.domain.blacklist.repository.BlackListRepository;
+import shop.donutmarket.donut.domain.board.model.Board;
+import shop.donutmarket.donut.domain.board.model.Event;
+import shop.donutmarket.donut.domain.board.repository.BoardRepository;
+import shop.donutmarket.donut.domain.board.repository.EventRepository;
 import shop.donutmarket.donut.domain.main.dto.MainResp;
+import shop.donutmarket.donut.domain.myLocation.model.MyLocation;
 import shop.donutmarket.donut.domain.myLocation.repository.MyLocationRepository;
+import shop.donutmarket.donut.domain.payment.model.Payment;
+import shop.donutmarket.donut.domain.payment.repository.PaymentRepository;
+import shop.donutmarket.donut.domain.report.model.Report;
+import shop.donutmarket.donut.domain.report.repository.ReportRepository;
 import shop.donutmarket.donut.domain.review.model.Rate;
+import shop.donutmarket.donut.domain.review.model.Review;
 import shop.donutmarket.donut.domain.review.repository.RateRepository;
+import shop.donutmarket.donut.domain.review.repository.ReviewRepository;
+import shop.donutmarket.donut.domain.user.model.User;
 import shop.donutmarket.donut.domain.user.repository.UserRepository;
 import shop.donutmarket.donut.global.dummy.DummyEntity;
 
@@ -52,14 +67,41 @@ public class MainControllerTest extends MyRestDocs {
     @Autowired
     private RateRepository rateRepository;
     @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private EventRepository eventRepository;
+    @Autowired
+    private BoardRepository boardRepository;
+    @Autowired
     private EntityManager em;
 
     @BeforeEach
     public void setUp() {
         Rate rate = Rate.builder().rateName("글레이즈드").createdAt(LocalDateTime.now()).build();
         rateRepository.save(rate);
-        userRepository.save(dummy.newUser("ssar@naver.com", "쌀", rate));
-        userRepository.save(dummy.newUser("cos@naver.com", "쌀", rate));
+        User user1 = userRepository.save(dummy.newUser("ssar@naver.com", "쌀", rate));
+        User user2 = userRepository.save(dummy.newUser("cos@naver.com", "쌀", rate));
+
+        // 카테고리 객체
+        Category category = Category.builder().name("생활가전").createdAt(LocalDateTime.now()).build();
+        categoryRepository.save(category);
+
+        // 내 지역
+        MyLocation myLocation = MyLocation.builder().user(user1).state("창원시").city("성산구").town("남양동").build();
+        myLocationRepository.save(myLocation);
+
+        // 이벤트 객체
+        Event event = Event.builder().latitude(111.111).longtitude(222.222).qty(1).paymentType("직거래")
+                .startAt(LocalDateTime.of(2023, 5, 1, 13, 30))
+                .endAt(LocalDateTime.of(2023, 5, 2, 13, 30))
+                .price(1000).createdAt(LocalDateTime.now()).build();
+        eventRepository.save(event);
+
+        // 게시글 객체
+        Board board = Board.builder().category(category).event(event)
+                .organizer(user1).title("제목1").content("내용1").img("사진").statusCode(200)
+                .views(100).recommend(false).state("부산광역시").city("부산진구").town("부전동").build();
+        boardRepository.save(board);
 
         em.clear();
     }
@@ -78,9 +120,9 @@ public class MainControllerTest extends MyRestDocs {
         System.out.println("테스트 : " + responseBody);
 
         // then
-//        resultActions.andExpect(jsonPath("$.blacklist.user.username").value("ssar@naver.com"));
-//        resultActions.andExpect(jsonPath("$.blacklist.blockedUser.username").value("cos@naver.com"));
-//        resultActions.andExpect(status().isOk());
-//        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+        resultActions.andExpect(jsonPath("$.myLocation.town").value("남양동"));
+        resultActions.andExpect(jsonPath("$.boards[0].title").value("제목1"));
+        resultActions.andExpect(status().isOk());
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
     }
 }
