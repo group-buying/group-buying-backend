5b2d594bb6d3e875f20e99a927f952b32836907a
Hoseok Seong <shsshs0125@naver.com>
2023-05-02 12:04:36 +0900

    Test: Report 통합테스트 완료

--- src/main/java/shop/donutmarket/donut/domain/report/service/ReportService.java
+++ src/main/java/shop/donutmarket/donut/domain/report/service/ReportService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+import shop.donutmarket.donut.domain.board.model.Board;
+import shop.donutmarket.donut.domain.board.repository.BoardRepository;
 import shop.donutmarket.donut.domain.report.dto.ReportReq;
 import shop.donutmarket.donut.domain.report.dto.ReportResp;
 import shop.donutmarket.donut.domain.report.model.Report;
@@ -23,6 +25,7 @@ public class ReportService {
 
     private final ReportRepository reportRepository;
     private final UserRepository userRepository;
+    private final BoardRepository boardRepository;
 
     @Transactional
     public ReportResp.InsertRespDTO 신고하기(MyUserDetails myUserDetails, Long reportedId, ReportReq.insertDTO insertDTO) {
@@ -33,13 +36,20 @@ public class ReportService {
             throw new Exception404("존재하지 않는 사용자입니다");
         }
 
+        Optional<Board> boardOP = boardRepository.findByIdWithAll(insertDTO.getBoardId());
+
+        if (boardOP.isEmpty()) {
+            throw new Exception404("존재하지 않는 게시글입니다");
+        }
+
         try {
             User reporterPS = myUserDetails.getUser();
             User reportedPS = reportedOP.get();
+            Board boardPS = boardOP.get();
             Report report = Report.builder()
                     .reporter(reporterPS)
                     .reported(reportedPS)
-                    .board(insertDTO.getBoard())
+                    .board(boardPS)
                     .title(insertDTO.getTitle())
                     .content(insertDTO.getContent())
                     .reportType(insertDTO.getReportType())
