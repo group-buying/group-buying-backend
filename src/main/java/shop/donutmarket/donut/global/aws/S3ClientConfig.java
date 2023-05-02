41bb58b52115eba56700d38b2d3caeb5a5d88075
K-Minho <alsgh9979@naver.com>
2023-05-02 18:34:39 +0900

    Feat: S3 config 추가

---
+++ src/main/java/shop/donutmarket/donut/global/aws/S3ClientConfig.java
@@ -0,0 +1,31 @@
+package shop.donutmarket.donut.global.aws;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class S3ClientConfig {
+
+    private String accessKey = System.getenv("AWS_ACCESS_KEY");
+    private String secretKey = System.getenv("AWS_SECRET_KEY");
+    private String region = "ap-northeast-2";
+
+    @Bean
+    AmazonS3 amazonS3Client() {
+        
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(this.region)
+                .build();
+    }
+}
