2a9237a5dd325cc1a2acdd505f0a6c1b69e47b54
K-Minho <alsgh9979@naver.com>
2023-05-03 15:28:46 +0900

    Feat: user bindingResult 작성 및 import 정리

--- src/main/java/shop/donutmarket/donut/domain/user/dto/UserResp.java
+++ src/main/java/shop/donutmarket/donut/domain/user/dto/UserResp.java
@@ -1,6 +1,5 @@
 package shop.donutmarket.donut.domain.user.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import shop.donutmarket.donut.domain.user.model.User;
