9d314066d7866e5f8879ceec0eaad2e654b023dd
K-Minho <alsgh9979@naver.com>
2023-05-03 14:48:33 +0900

    Feat: chat bindingResult 작성

--- src/main/java/shop/donutmarket/donut/domain/chat/model/Chatroom.java
+++ src/main/java/shop/donutmarket/donut/domain/chat/model/Chatroom.java
@@ -2,18 +2,18 @@ package shop.donutmarket.donut.domain.chat.model;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
 import shop.donutmarket.donut.domain.board.model.Event;
 
 @Getter
