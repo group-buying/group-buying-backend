68e4637e26793d02d4e48acbdaecc6ccc4470201
K-Minho <alsgh9979@naver.com>
2023-05-03 15:23:14 +0900

    Feat: review bindingResult 작성 및 import 정리

--- src/main/java/shop/donutmarket/donut/domain/review/controller/ReviewController.java
+++ src/main/java/shop/donutmarket/donut/domain/review/controller/ReviewController.java
@@ -1,24 +1,19 @@
 package shop.donutmarket.donut.domain.review.controller;
 
-import java.util.List;
-
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import shop.donutmarket.donut.domain.review.dto.ReviewReq.ReviewSaveReqDTO;
 import shop.donutmarket.donut.domain.review.dto.ReviewResp.ReviewSaveRespDTO;
-import shop.donutmarket.donut.domain.review.model.Review;
 import shop.donutmarket.donut.domain.review.service.ReviewService;
 import shop.donutmarket.donut.global.auth.MyUserDetails;
-import shop.donutmarket.donut.global.dto.ResponseDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,7 +23,7 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping
-    public ResponseEntity<?> save(@RequestBody @Valid ReviewSaveReqDTO reviewSaveReqDTO, @AuthenticationPrincipal MyUserDetails myUserDetails) {
+    public ResponseEntity<?> save(@RequestBody @Valid ReviewSaveReqDTO reviewSaveReqDTO, BindingResult bindingResult, @AuthenticationPrincipal MyUserDetails myUserDetails) {
         ReviewSaveRespDTO saveRespDTO = reviewService.리뷰작성(reviewSaveReqDTO, myUserDetails);
         return ResponseEntity.ok(saveRespDTO);
     }
--- src/main/java/shop/donutmarket/donut/domain/review/repository/RateRepository.java
+++ src/main/java/shop/donutmarket/donut/domain/review/repository/RateRepository.java
@@ -1,10 +1,6 @@
 package shop.donutmarket.donut.domain.review.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import shop.donutmarket.donut.domain.review.model.Rate;
 
