782c6ade7ccc6ef81ad6ce60bece860c8798bb0b
Hoseok Seong <shsshs0125@naver.com>
2023-05-06 12:21:51 +0900

    Test: Board 게시글 작성 테스트 완료

--- src/test/java/shop/donutmarket/donut/integration/board/BoardControllerTest.java
+++ src/test/java/shop/donutmarket/donut/integration/board/BoardControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.TestExecutionEvent;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
@@ -19,13 +20,20 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
 import shop.donutmarket.donut.core.MyRestDocs;
+import shop.donutmarket.donut.domain.admin.model.Category;
+import shop.donutmarket.donut.domain.admin.repository.CategoryRepository;
 import shop.donutmarket.donut.domain.blacklist.repository.BlackListRepository;
+import shop.donutmarket.donut.domain.board.dto.BoardReq;
+import shop.donutmarket.donut.domain.board.repository.EventRepository;
 import shop.donutmarket.donut.domain.review.model.Rate;
 import shop.donutmarket.donut.domain.review.repository.RateRepository;
+import shop.donutmarket.donut.domain.user.model.User;
 import shop.donutmarket.donut.domain.user.repository.UserRepository;
 import shop.donutmarket.donut.global.dummy.DummyEntity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -49,14 +57,25 @@ public class BoardControllerTest extends MyRestDocs {
     @Autowired
     private RateRepository rateRepository;
     @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private EventRepository eventRepository;
+    @Autowired
     private EntityManager em;
 
     @BeforeEach
     public void setUp() {
+        categoryRepository.deleteAll();
+        eventRepository.deleteAll();
+
         Rate rate = Rate.builder().rateName("글레이즈드").createdAt(LocalDateTime.now()).build();
         rateRepository.save(rate);
-        userRepository.save(dummy.newUser("ssar@naver.com", "쌀", rate));
+        User user1 = userRepository.save(dummy.newUser("ssar@naver.com", "쌀", rate));
         userRepository.save(dummy.newUser("cos@naver.com", "쌀", rate));
+
+        // 카테고리 객체
+        Category category = Category.builder().name("편의점").createdAt(LocalDateTime.now()).build();
+        categoryRepository.save(category);
 
         em.clear();
     }
@@ -70,18 +89,38 @@ public class BoardControllerTest extends MyRestDocs {
     @Test
     public void save_test() throws Exception {
         // given
-        Long id = 2L;
+        BoardReq.BoardSaveReqDTO boardSaveReqDTO = new BoardReq.BoardSaveReqDTO();
+        boardSaveReqDTO.setCategoryId(1L);
+        boardSaveReqDTO.setTitle("제목");
+        boardSaveReqDTO.setContent("내용");
+        boardSaveReqDTO.setState("부산광역시");
+        boardSaveReqDTO.setCity("부산진구");
+        boardSaveReqDTO.setTown("부전동");
+        boardSaveReqDTO.setLatitude(111.222);
+        boardSaveReqDTO.setLongtitude(222.333);
+        boardSaveReqDTO.setQty(3);
+        boardSaveReqDTO.setPaymentType("직거래");
+        boardSaveReqDTO.setEndAt(LocalDateTime.of(2023, 5, 2, 13, 30));
+        boardSaveReqDTO.setPrice(1000);
+        List<String> comment = new ArrayList<>();
+        comment.add("댓글1");
+        comment.add("댓글2");
+        boardSaveReqDTO.setComment(comment);
+
+        String requestBody = om.writeValueAsString(boardSaveReqDTO);
 
         // when
         ResultActions resultActions = mvc
-                .perform(post("/blacklists/"+id));
+                .perform(post("/boards").content(requestBody).contentType(MediaType.APPLICATION_JSON));
 
         String responseBody = resultActions.andReturn().getResponse().getContentAsString();
         System.out.println("테스트 : " + responseBody);
 
         // then
-        resultActions.andExpect(jsonPath("$.data.blacklist.user.username").value("ssar@naver.com"));
-        resultActions.andExpect(jsonPath("$.data.blacklist.blockedUser.username").value("cos@naver.com"));
+        resultActions.andExpect(jsonPath("$.data.board.category.name").value("편의점"));
+        resultActions.andExpect(jsonPath("$.data.board.title").value("제목"));
+        resultActions.andExpect(jsonPath("$.data.board.organizer.name").value("쌀"));
+        resultActions.andExpect(jsonPath("$.data.board.event.paymentType").value("직거래"));
         resultActions.andExpect(status().isOk());
         resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
     }
