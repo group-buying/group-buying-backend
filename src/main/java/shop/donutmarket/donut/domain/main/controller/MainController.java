a286982ed4a2c378ac08afeb1c4a4beca57ba7b0
K-Minho <alsgh9979@naver.com>
2023-05-08 11:07:56 +0900

    Feat: 메인페이지 필터링 추가

--- src/main/java/shop/donutmarket/donut/domain/main/controller/MainController.java
+++ src/main/java/shop/donutmarket/donut/domain/main/controller/MainController.java
@@ -1,11 +1,16 @@
 package shop.donutmarket.donut.domain.main.controller;
 
+import java.util.List;
+
+import org.checkerframework.checker.units.qual.m;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import shop.donutmarket.donut.domain.board.model.Board;
 import shop.donutmarket.donut.domain.main.dto.MainResp;
 import shop.donutmarket.donut.domain.main.service.MainService;
 import shop.donutmarket.donut.global.auth.MyUserDetails;
@@ -21,6 +26,13 @@ public class MainController {
     public ResponseEntity<?> main(@AuthenticationPrincipal MyUserDetails myUserDetails) {
         MainResp mainResp = mainService.게시글목록보기(myUserDetails.getUser().getId());
         ResponseDTO<?> responseDTO = new ResponseDTO<>(mainResp);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @GetMapping("/main/{categoryId}")
+    public ResponseEntity<?> mainList(@AuthenticationPrincipal MyUserDetails myUserDetails, @PathVariable Long categoryId) {
+        List<Board> boardList = mainService.게시글필터(myUserDetails, categoryId); 
+        ResponseDTO<?> responseDTO = new ResponseDTO<>(boardList);
         return ResponseEntity.ok(responseDTO);
     }
 }
--- src/main/java/shop/donutmarket/donut/domain/main/service/MainService.java
+++ src/main/java/shop/donutmarket/donut/domain/main/service/MainService.java
@@ -1,7 +1,10 @@
 package shop.donutmarket.donut.domain.main.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +16,9 @@ import shop.donutmarket.donut.domain.myCategory.model.MyCategory;
 import shop.donutmarket.donut.domain.myCategory.repository.MyCategoryRepository;
 import shop.donutmarket.donut.domain.myLocation.model.MyLocation;
 import shop.donutmarket.donut.domain.myLocation.repository.MyLocationRepository;
+import shop.donutmarket.donut.domain.user.model.User;
+import shop.donutmarket.donut.global.auth.MyUserDetails;
+import shop.donutmarket.donut.global.exception.Exception404;
 import shop.donutmarket.donut.global.exception.Exception500;
 
 @RequiredArgsConstructor
@@ -32,6 +38,26 @@ public class MainService {
             List<Board> boards = boardRepository.findBoardAndStatusCode();
             MainResp mainResp = new MainResp(myCategoryListPS, myLocationDTO, boards);
             return mainResp;
+        } catch (Exception e) {
+            throw new Exception500("게시글 목록보기 실패 : " + e.getMessage());
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public List<Board> 게시글필터(@AuthenticationPrincipal MyUserDetails myUserDetails, Long categoryId) {
+        try {
+            User user = myUserDetails.getUser();
+            Optional<MyLocation> userLocationOP = myLocationRepository.findByUserId(user.getId());
+            if (userLocationOP.isEmpty()) {
+                throw new Exception404("내 위치가 없습니다.");
+            }
+            MyLocation userLocationPS = userLocationOP.get();
+
+            List<Board> boardResult = boardRepository.findByMain(userLocationPS.getState(), userLocationPS.getCity(), userLocationPS.getTown(), categoryId);
+            if (boardResult.isEmpty()) {
+                throw new Exception404("검색에 맞는 결과가 없습니다");
+            }
+            return boardResult;
         } catch (Exception e) {
             throw new Exception500("게시글 목록보기 실패 : " + e.getMessage());
         }
