99f3ad2f74bcbfbfa4b534d827c26710a16caaf1
K-Minho <alsgh9979@naver.com>
2023-05-08 11:08:15 +0900

    Debug: 일부 검색에 대한 필터링 수정

--- src/main/java/shop/donutmarket/donut/domain/board/repository/BoardRepository.java
+++ src/main/java/shop/donutmarket/donut/domain/board/repository/BoardRepository.java
@@ -26,13 +26,13 @@ public interface BoardRepository extends JpaRepository<Board, Long>{
     @Query("select b from Board b where b.statusCode in(200, 201, 202)")
     List<Board> findBoardAndStatusCode();
 
-    @Query("select b.id FROM Board b where (b.id IN (SELECT b2.id FROM Board b2 WHERE b2.title LIKE %:searchWord%) OR b.id IN (SELECT t.boardId FROM Tag t WHERE t.comment LIKE %:searchWord%)) AND b.statusCode IN (200, 201, 202)")
+    @Query("select b.id FROM Board b where (b.id IN (SELECT b2.id FROM Board b2 WHERE b2.title LIKE %:searchWord% or b2.content like %:searchWord%) OR b.id IN (SELECT t.boardId FROM Tag t WHERE t.comment LIKE %:searchWord%)) AND b.statusCode IN (200, 201, 202)")
     List<Long> findIdsBySearchWord(@Param("searchWord") String searchWord);
     
     @Query("select b.id from Board b where b.state = :state and b.city = :city and b.town = :town and b.statusCode in(200, 201, 202)")
     List<Long> findByLocation(@Param("state") String state, @Param("city") String city, @Param("state") String town);
 
-    @Query("select b from Board b where b.category.id = :categoryId and b.statusCode in(200, 201, 202)")
+    @Query("select b from Board b left join fetch b.category c left join fetch b.event e left join fetch b.organizer o left join fetch o.rate where b.category.id = :categoryId and b.statusCode in(200, 201, 202)")
     List<Board> findByCategory(@Param("categoryId") Long categoryId);
 
     @Query("select b from Board b left join fetch b.category c left join fetch b.event e left join fetch b.organizer o left join fetch o.rate where b.state = :state and b.city = :city and b.town = :town and b.category.id = :categoryId and b.statusCode in(200, 201, 202)")
